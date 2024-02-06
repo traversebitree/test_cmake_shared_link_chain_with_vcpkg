946419c61220c4d29f6fa8f25bdfad36d2942f0a
traversebitree <traversebitree@users.noreply.github.com>
2024-02-06 06:12:27 +0000

    Automated commit of cmake-clang-format changes.

--- dylibsrc/dylibsrc.cpp
+++ dylibsrc/dylibsrc.cpp
@@ -6,6 +6,7 @@
 #include <dylibsrc.hpp>
 #include <fmt/core.h>
 
-void print_calc_plus_ab(int a, int b) {
-  fmt::print("Result of {} + {} is {}.n", a, b, a + b);
+void print_calc_plus_ab(int a, int b)
+{
+    fmt::print("Result of {} + {} is {}.n", a, b, a + b);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 
 using namespace std;
 
-int main(int argc, char **argv) {
-  cout << "hello world!" << endl;
-  print_calc_plus_ab(3, 4);
-  // fmt::print("{}\n", "Print from fmt...");
-  return 0;
+int main(int argc, char **argv)
+{
+    cout << "hello world!" << endl;
+    print_calc_plus_ab(3, 4);
+    // fmt::print("{}\n", "Print from fmt...");
+    return 0;
 }
